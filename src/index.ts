2ca04a46daae056b87439a66106050bc58255f10
Emilio Martinez <e.marticor@gmail.com>
2018-10-28 23:42:38 -0700

    feat: support callbacks and Promises simultaneously, if desired

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,56 @@
 type Callback<T> = (value: T) => void;
+type StoredCallbacks = { resolve: Callback<null>; reject: Callback<Error> };
+
+const callbackMap: { [src: string]: StoredCallbacks[] } = {};
+
+function addCallbacksFor(src: string, callbacks: StoredCallbacks) {
+  callbackMap[src] = (callbackMap[src] || []).concat(callbacks);
+}
+
+function runCallbacksFor(src: string, success: boolean) {
+  let i = callbackMap[src].length;
+  while (i--) {
+    const cb = callbackMap[src].pop()!;
+    success ? cb.resolve(null) : cb.reject(new Error(`Failed to load script at ${src}`));
+  }
+}
+
+function loadHandler(
+  script: HTMLScriptElement,
+  src: string,
+  resolve: Callback<null>,
+  reject: Callback<Error>
+) {
+  addCallbacksFor(src, { resolve, reject });
+
+  script.onload = () => {
+    script.onerror = script.onload = null as any;
+    runCallbacksFor(src, true);
+  };
+  script.onerror = () => {
+    script.onerror = script.onload = null as any;
+    runCallbacksFor(src, false);
+  };
+}
 
 export function loadScript(src: string, cb?: Callback<Error | null>) {
   const script = document.createElement('script');
   script.type = 'text/javascript';
   script.async = true;
-  script.src = `${src}`;
-
-  const handler = (resolve: Callback<null>, reject: Callback<Error>) => {
-    script.onload = () => {
-      script.onerror = script.onload = null as any;
-      resolve(null);
-    };
-    script.onerror = () => {
-      script.onerror = script.onload = null as any;
-      reject(new Error(`Failed to load script at ${src}`));
-    };
-  };
 
   setTimeout(() => {
     const firstScript = document.scripts[0];
     firstScript.parentNode!.insertBefore(script, firstScript);
+    script.src = `${src}`;
   }, 0);
 
   // Handle callback, if available
   if (typeof cb == 'function') {
-    return handler(cb, cb);
+    loadHandler(script, src, cb, cb);
   }
 
   // If Promise is available, return Promise
   if (typeof Promise == 'function') {
-    return new Promise<null>((resolve, reject) => handler(resolve, reject));
+    return new Promise<null>((resolve, reject) => loadHandler(script, src, resolve, reject));
   }
 }
