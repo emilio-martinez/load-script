c510a0d3c145271a34fb40f45041d55538c30595
Emilio Martinez <e.marticor@gmail.com>
2018-10-29 12:10:57 -0700

    test: add initial test suite

---
+++ karma.conf.js
@@ -0,0 +1,35 @@
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
+module.exports = function(config) {
+  config.set({
+    frameworks: ['jasmine', 'karma-typescript'],
+    files: [
+      '+(src|test)/**/*.ts',
+      {
+        pattern: 'test/scripts/*',
+        included: false
+      }
+    ],
+    preprocessors: {
+      '**/*.ts': 'karma-typescript'
+    },
+    reporters: ['progress', 'karma-typescript'],
+    karmaTypescriptConfig: {
+      tsconfig: './tsconfig-spec.json'
+    },
+    customLaunchers: {
+      ChromeHeadlessWithoutSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+    browsers: ['ChromeHeadlessWithoutSandbox'],
+    singleRun: true,
+    coverageOptions: {
+      reports: {
+        html: 'coverage',
+        'text-summary': null
+      }
+    }
+  });
+};
